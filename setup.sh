c5a8e9f0af23bf4feed1441ab47f1639cb8fa28b
David Albert <davidbalbert@gmail.com>
2015-08-18 12:44:00 -0400

    Basic setup script

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+
+pushd `dirname $0` > /dev/null
+DIR=`pwd`
+popd > /dev/null
+
+DOTFILES="
+  bash_profile
+  bashrc
+  ctags
+  editrc
+  gemrc
+  gvimrc
+  inputrc
+  ocamlinit
+  tmux.conf
+  vimrc
+  vim
+"
+
+NON_DOTFILES="
+  lib
+"
+
+for f in $DOTFILES; do
+  ln -s $DIR/$f ~/.$f
+done
+
+for f in $NON_DOTFILES; do
+  ln -s $DIR/$f ~/$f
+done
+
+mkdir -p ~/bin
+
+for f in `ls $DIR/bin`; do
+  ln -s $DIR/bin/$f ~/bin/$f
+done
